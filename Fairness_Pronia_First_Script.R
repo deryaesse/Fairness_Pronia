046d299760555da7495fc1c126c99d4a01d2ee8f
deryaesse <deryasahin@protonmail.ch>
2022-03-14 10:06:19 +0100

    sex and education fairness criteria

--- Fairness_Pronia_First_Script.R
+++ Fairness_Pronia_First_Script.R
@@ -39,8 +39,13 @@ pronia <- pronia %>% mutate(
   Ethnicity = if_else(Ethnicity=="Finnish", "FI", Ethnicity),
   Ethnicity = if_else(Ethnicity=="Turkish", "TR", Ethnicity),
   Ethnicity = if_else(Ethnicity=="Italian", "IT", Ethnicity),
-  Ethnicity = if_else(Ethnicity=="British", "UK", Ethnicity)
+  Ethnicity = if_else(Ethnicity=="British", "UK", Ethnicity),
 )
+
+pronia <- pronia %>% mutate(
+  AGE_T0= as.numeric(AGE_T0),
+  AGE_T0 = ifelse(AGE_T0>100, AGE_T0/1000, AGE_T0),
+  edstat = ifelse(Education > median(Education, na.rm=TRUE), "high", "low"))
 
 pronia <- pronia %>% mutate(
   PROGNOSTIC_01_01_Transition_T0 = as.factor(PROGNOSTIC_01_01_Transition_T0),
@@ -48,7 +53,6 @@ pronia <- pronia %>% mutate(
   Studygroup = as.factor(Studygroup),
   PoB_Country = as.factor(PoB_Country),
   SEX = as.factor(SEX),
-  AGE_T0 = as.numeric(AGE_T0),
   Education = as.numeric(Education),
   Ethnicity = as.factor(Ethnicity),
   CurrentCountry = as.factor(CurrentCountry),
@@ -59,5 +63,126 @@ pronia <- pronia %>% mutate(
 
 pronia_chr <- pronia %>%  filter(Studygroup=="CHR")
 
+pronia_transition <- inner_join(pronia_chr, transition_models, by="PSN")
+
+### Fairness Selected Models (Carrion, Salokangas, Cornblatt, Haidl, Clinician Rating)
+
+sensitive_attributes <- c("SEX", "edstat")
+models <- c("transition", "carrion", "salokangas", "cornblatt", "haidl", "PROGNOSTIC_01_01_Transition_T0")
+
+res_carrion <- pronia_transition %>% 
+  select("PSN", sensitive_attributes, models) %>% 
+  mutate(
+    TP=ifelse(transition==1 & carrion==1, 1,0),
+    TN=ifelse(transition==0 & carrion==0, 1,0),
+    FP=ifelse(transition==0 & carrion==1, 1,0),
+    FN=ifelse(transition==1 & carrion==0, 1,0)) #%>%
+ # summarize_at(
+  #  vars(TP, TN, FP, FN), funs(sum) )
+
+res_salokangas <- pronia_transition %>% 
+  select("PSN", sensitive_attributes, models) %>% 
+  mutate(
+    TP=ifelse(transition==1 & salokangas==1, 1,0),
+    TN=ifelse(transition==0 & salokangas==0, 1,0),
+    FP=ifelse(transition==0 & salokangas==1, 1,0),
+    FN=ifelse(transition==1 & salokangas==0, 1,0)) #%>%
+#  summarize_at(
+ #   vars(TP, TN, FP, FN), funs(sum))
+
+res_haidl <- pronia_transition %>% 
+  select("PSN", sensitive_attributes, models) %>% 
+  mutate(
+    TP=ifelse(transition==1 & haidl==1, 1,0),
+    TN=ifelse(transition==0 & haidl==0, 1,0),
+    FP=ifelse(transition==0 & haidl==1, 1,0),
+    FN=ifelse(transition==1 & haidl==0, 1,0))# %>%
+ # summarize_at(
+  #  vars(TP, TN, FP, FN), funs(sum))
+
+res_cornblatt <- pronia_transition %>% 
+  select("PSN", sensitive_attributes, models) %>% 
+  mutate(
+    TP=ifelse(transition==1 & cornblatt==1, 1,0),
+    TN=ifelse(transition==0 & cornblatt==0, 1,0),
+    FP=ifelse(transition==0 & cornblatt==1, 1,0),
+    FN=ifelse(transition==1 & cornblatt==0, 1,0)) #%>%
+  #summarize_at(
+   # vars(TP, TN, FP, FN), funs(sum))
+
+res_clinician <- pronia_transition %>% 
+  select("PSN", sensitive_attributes, models) %>% 
+  mutate(
+    TP=ifelse(transition==1 & PROGNOSTIC_01_01_Transition_T0==1, 1,0),
+    TN=ifelse(transition==0 & PROGNOSTIC_01_01_Transition_T0==0, 1,0),
+    FP=ifelse(transition==0 & PROGNOSTIC_01_01_Transition_T0==1, 1,0),
+    FN=ifelse(transition==1 & PROGNOSTIC_01_01_Transition_T0==0, 1,0)) #%>%
+ # summarize_at(
+#    vars(TP, TN, FP, FN), funs(sum))
+
+sex <- rbind(res_cornblatt %>% mutate(Model='Cornblatt'),
+             res_carrion %>% mutate(Model='Carrion'),
+             res_salokangas %>% mutate(Model="Salokangas"),
+             res_clinician %>% mutate(Model="Clinician")
+) %>%
+  group_by(Model, SEX) %>%
+  filter(!is.na(SEX)) %>%
+  summarize_at(
+    vars(TP, TN, FP, FN), funs(mean)
+  )%>%
+  mutate(
+    "Accuracy Equality" = (TP+TN)/(TP+FP+TN+FN),
+    "Equal Opportunity" = FN/(TP+FN),
+    "Predictive Equality" = FP/(FP+TN),
+    "Predictive Parity" = TP/(TP+FP),
+    "Treatment Equality" = (FN/FP),
+    "MCC Parity" = abs((((TP * TN) - (FP * FN)) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))))
+  ) %>%
+  select(-c(TP, FP, TN, FN)) %>%
+  pivot_longer(-c(Model, SEX)) %>%
+  pivot_wider(names_from = SEX, values_from=value) %>%
+  mutate(ratio = `male`/`female`) %>%
+  ggplot(aes(y=ratio, x=Model, fill=Model)) + geom_col(stat='identity', position=position_dodge()) + 
+  theme_minimal() + 
+  geom_hline(yintercept = 0.8, lty=2) + 
+  geom_hline(yintercept = 1.25, lty=2) + 
+  scale_fill_grey() + 
+  facet_grid(.~name, labeller=label_wrap_gen(width = 10, multi_line = TRUE)) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank()) +
+  ggtitle(label = 'Fairness criteria for sensitive attribute: Sex', 
+   subtitle = "(Protected Group: Male)")
 
 
+education <- rbind(res_cornblatt %>% mutate(Model='Cornblatt'),
+             res_carrion %>% mutate(Model='Carrion'),
+             res_salokangas %>% mutate(Model="Salokangas"),
+             res_clinician %>% mutate(Model="Clinician")
+) %>%
+  group_by(Model, edstat) %>%
+  filter(!is.na(edstat)) %>%
+  summarize_at(
+    vars(TP, TN, FP, FN), funs(mean)
+  )%>%
+  mutate(
+    "Accuracy Equality" = (TP+TN)/(TP+FP+TN+FN),
+    "Equal Opportunity" = FN/(TP+FN),
+    "Predictive Equality" = FP/(FP+TN),
+    "Predictive Parity" = TP/(TP+FP),
+    "Treatment Equality" = (FN/FP),
+    "MCC Parity" = abs((((TP * TN) - (FP * FN)) / sqrt((TP + FP) * (TP + FN) * (TN + FP) * (TN + FN))))
+  ) %>%
+  select(-c(TP, FP, TN, FN)) %>%
+  pivot_longer(-c(Model, edstat)) %>%
+  pivot_wider(names_from = edstat, values_from=value) %>%
+  mutate(ratio = `high`/`low`) %>%
+  ggplot(aes(y=ratio, x=Model, fill=Model)) + geom_col(stat='identity', position=position_dodge()) + 
+  theme_minimal() + 
+  geom_hline(yintercept = 0.8, lty=2) + 
+  geom_hline(yintercept = 1.25, lty=2) + 
+  scale_fill_grey() + 
+  facet_grid(.~name, labeller=label_wrap_gen(width = 10, multi_line = TRUE)) +
+  theme(axis.title.x=element_blank(),
+        axis.text.x=element_blank()) +
+  ggtitle(label = 'Fairness criteria for sensitive attribute: Education', 
+   subtitle = "(Protected Group: High)")
